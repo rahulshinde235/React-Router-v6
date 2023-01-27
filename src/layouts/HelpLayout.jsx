57a19a310883c34b070cb04beefd04e52b6f44cc
Rahul <rahul@storyxpress.co>
2023-01-27 19:21:44 +0530

    Learned layouts and nested routing

---
+++ src/layouts/HelpLayout.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink, Outlet } from 'react-router-dom'
+
+export const HelpLayout = () => {
+  return (
+    <div class="help-layout">
+        <h2>Website Help</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea molestias, saepe doloremque vel doloribus soluta amet. Porro, blanditiis commodi rerum corporis a laudantium perspiciatis magnam eligendi suscipit explicabo deserunt soluta!</p>
+        <nav>
+            <NavLink to="faq">View the FAQ</NavLink>
+            <NavLink to="contact">Contact Us</NavLink>
+        </nav>
+
+        <Outlet/>
+    </div>
+    )
+}
